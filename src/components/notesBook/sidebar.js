15696eb9518541fc57a4ff30150af429ae0651f7
Noe Oziel Perez Lopez <ploz2767@gmail.com>
2023-07-13 18:21:25 -0600

    fixed bottom nav

--- src/components/notesBook/sidebar.js
+++ src/components/notesBook/sidebar.js
@@ -7,7 +7,7 @@ export default function SideBar({ children }) {
   return active === false ? (
     <div
       onClick={() => setActive(true)}
-      className="h-[50px] w-[50px] absolute z-20 bottom-7 left-5 rounded-full bg-slate-900"
+      className="h-[50px] w-[50px] absolute z-20 bottom-12 left-5 rounded-full bg-slate-900"
     ></div>
   ) : (
     <nav className="w-[45%] h-full sm:w-[45%] md:w-[45%] lg:w-[20%] bg-slate-900 sm:bg-slate-900 md:bg-slate-900 lg:bg-transparent z-20 absolute sm:top-0 sm:left-0 md:top-0 md:left-0 sm:absolute md:absolute lg:relative lg:self-center lg:h-5/6 max-h-screen overflow-y-auto flex flex-col grow">
